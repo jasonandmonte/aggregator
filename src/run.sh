61a903d31c7baf9689e585cb4df368ae70dc5bf2
jbot <jason.monte55@gmail.com>
2025-04-05 21:09:53 -0700

    Automated update

--- src/run.sh
+++ src/run.sh
@@ -3,9 +3,11 @@
 cd /home/jasonandmonte/projects/aggregator
 git pull
 
+echo "Starting scrape"
 source env/bin/activate
 python3 src/scraper.py > ../index.html
 deactivate
+echo "Committing"
 
 git add .
 git commit -m "Automated update"
